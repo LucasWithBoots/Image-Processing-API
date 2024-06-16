86cb124b39223c3a3ced9a9d702164e12c3c7c92
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2024-06-16 11:43:41 -0300

    feat: sistema básico funcionando

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import sharp from 'sharp';
+import * as fs from "node:fs";
 
 const image = express.Router();
 
@@ -7,12 +9,36 @@ const image = express.Router();
 //   next()
 // })
 
-image.get("/:nomeImagem", (req, res,next) => {
-  const nomeImagem = req.params.nomeImagem;
-  console.log(nomeImagem);
-  next()
-})
+// http://localhost:3000/api/image/?nomeImagem=palmtunnel.jpg&width=300&height=300
 
-image.use(express.static('public'));
+image.get("/", (req, res,next) => {
+  const { fileName, width, height } = req.query
+  console.log(`${fileName} ${width} ${height}`);
+
+  let inputBuffer = `./public/full/${fileName}`;
+  let outputBuffer = `./public/thumb/${fileName}`;
+
+  sharp(inputBuffer)
+    .resize(+width, +height)
+    .toFile(outputBuffer, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Erro ao processar a imagem.');
+        return;
+      }
+
+      fs.readFile(outputBuffer, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Erro ao ler a imagem processada.');
+          return;
+        }
+
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(data);
+      });
+    });
+});
+
 
 export default image
